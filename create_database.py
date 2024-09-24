a0569c0d687da61f2eb9d9636f4bc73670987b69
Joanna Farris <joannafarris@gmail.com>
2024-09-24 03:12:15 -0500

    created database

---
+++ create_database.py
@@ -0,0 +1,22 @@
+import sqlite3
+import pandas as pd
+import pathlib
+
+# Define the database file in the current root project directory
+db_file = pathlib.Path("project.sqlite3")
+
+def create_database():
+    """Function to create a database."""
+    try:
+        conn = sqlite3.connect(db_file)
+        conn.close()
+        print("Database created successfully.")
+    except sqlite3.Error as e:
+        print("Error creating the database:", e)
+
+def main():
+    create_database()
+
+if __name__ == "__main__":
+    main()
+    
